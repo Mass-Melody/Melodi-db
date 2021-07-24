e8c86fa45dd494ae118954f4329ccb947b07faf6
Jenner Dulce <jennerdulce@gmail.com>
2021-07-24 13:40:31 -0700

    Updated Schema

--- src/models/allUsers/model.js
+++ src/models/allUsers/model.js
@@ -12,15 +12,67 @@ const allUsersSchema = mongoose.Schema({
   picture: { String, default: ''},
   hero: {type: String, default: ''},
   //json string object representing array of friends
-  friends: {type: String, default: ''},
+  friends: {type: String, default: '[]'},
   aboutMe: {type: String, default: ''},
   playlist: {type: String, default: ''},
   //json string object representing array of user's posts
-  posts: {type: String, default: ''},
+  posts: {type: String, default: '[]'},
   //json string object representing array of user's interests
-  interests: {type: String, default: ''},
+  interests: {type: String, 
+    default: `
+    [
+      {
+        id: 1,
+        name: 'General',
+        info: '---'
+      },
+      {
+        id: 2,
+        name: 'Music Genre',
+        info: '---'
+      },
+      {
+        id: 3,
+        name: 'Instrument',
+        info: '---'
+      },
+      {
+        id: 4,
+        name: 'Books',
+        info: '---'
+      }
+    ]
+    `},
   //json string object representing array of user's details?
-  details: {type: String, default: ''}
+  details: {type: String,  default: `
+  [
+    {
+      id: 1,
+      name: 'Gender',
+      info: '---'
+    },
+    {
+      id: 2,
+      name: 'Hometown',
+      info: '---'
+    },
+    {
+      id: 3,
+      name: 'Food',
+      info: '---'
+    },
+    {
+      id: 4,
+      name: 'Bands',
+      info: '---'
+    },
+    {
+      id: 5,
+      name: 'Hobbies',
+      info: '---'
+    }
+  ]
+  `}
 })
 
 const allUsersModel = mongoose.model('allUsers', allUsersSchema);
