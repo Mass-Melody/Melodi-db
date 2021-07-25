ffc41898fe09bae47aa6cc8ce27f78114d0d256b
Jenner Dulce <jennerdulce@gmail.com>
2021-07-25 11:40:52 -0700

    updated

--- src/server.js
+++ src/server.js
@@ -28,10 +28,10 @@ app.use(v2Routes);
 
 // Home Handler
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next()
-});
-
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 app.get('/', (req, res) => {
   res.status(200).json('Server is Working!')
 })
